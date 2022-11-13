d8a5a27634cd4b649802f338f5c096e7e8c24a98
Arvid <axar1802@student.ju.se>
2022-11-13 15:23:43 +0100

    Initial Commit

---
+++ ExampleQuiz/ExampleQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleQuizApp.swift
+//  ExampleQuiz
+//
+//  Created by Arvid Axelsson on 2022-11-13.
+//
+
+import SwiftUI
+
+@main
+struct ExampleQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
